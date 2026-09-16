d909c3d2dedeff7a4b9f0ef3adfd391489363156
CongTuanND <congtuan.nd89@gmail.com>
2026-09-16 17:54:08 +0000

    Attach Q&A and  complete solution for bit operation excercises

--- bit_operations.c
+++ bit_operations.c
@@ -69,8 +69,97 @@ int main() {
                 }
                 printf("Number of set bits in %d: %d\n", a, count);
             }
+            break;
+
+            case 6: 
+            {
+                if (a > 0 && (a & (a - 1)) == 0)
+                    printf("%d is a power of two\n", a);
+                else
+                    printf("%d is not a power of two\n", a);
+            }
+            break;
+
+            case 7: 
+            {
+                int twos_complement = ~a + 1;
+                printf("2's complement of %d: %d\n", a, twos_complement);
+            }
+            break;
+
+            case 8: 
+            {
+                if (a & 1)
+                    printf("%d is odd\n", a);
+                else
+                    printf("%d is even\n", a);
+            }
+            break;
+
+            case 9: 
+            {
+                int msb_position = -1;
+                for (int i = 31; i >= 0; i--) 
+                {
+                    if (a & (1 << i)) 
+                    {
+                        msb_position = i;
+                        break;
+                    }
+                }
+                if (msb_position != -1)
+                    printf("Most significant bit set in %d is at position %d\n", a, msb_position);
+                else
+                    printf("No bits are set in %d\n", a);
+            }
+            break;
+
+            case 10: 
+            {
+                int lsb_position = -1;
+                for (int i = 0; i < 32; i++) 
+                {
+                    if (a & (1 << i)) 
+                    {
+                        lsb_position = i;
+                        break;
+                    }
+                }
+                if (lsb_position != -1)
+                    printf("Least significant bit set in %d is at position %d\n", a, lsb_position);
+                else
+                    printf("No bits are set in %d\n", a);
+            }
+            break;
+
+            case 11: 
+            {
+                a &= (a - 1);
+                printf("After clearing the rightmost set bit: %d\n", a);
+            }
+            break;
+
+            case 12: 
+            {
+                int rightmost_set_bit = a & -a;
+                printf("Rightmost set bit in %d is: %d\n", a, rightmost_set_bit);
+            }
+            break;
+
+            case 13: 
+            {
+                if (a % 8 == 0)
+                    printf("%d has 8 as a factor\n", a);
+                else
+                    printf("%d does not have 8 as a factor\n", a);
+            }
+            break;
+
             default:
+            {
                 printf("Invalid choice\n");
+            } 
+            break;
         }
         printf("Do you want to perform another operation? (1 for yes, 0 for no): ");
         scanf("%d", &new_operation);
